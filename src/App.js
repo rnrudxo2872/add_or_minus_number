a5fc45f83012b8b5577d9da23c9fc39c13bea56f
unknown <rnrudxo2872@naver.com>
2021-03-10 23:51:52 +0900

    Plus and Minus

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
 import React,{Component} from 'react'
-import data from "./test.json"
-import Sub from "./test2"
 
 class App extends Component {
+  state ={ // counter
+    AppNumber:0
+  }
+  add = () => {
+    this.setState({
+        AppNumber: this.state.AppNumber + 1
+      })
+    }
+  minus = () =>{    
+    this.setState({
+      AppNumber: this.state.AppNumber - 1
+    })
+  }
   render(){
-    
     return(
       <div className="App">
-      <h1>hi</h1>
-      {data.map(some =><Sub key={some.id} food={some.food} desc={some.desc}/>)}
-      
+      <h1>CurNumber : {this.state.AppNumber}</h1>
+      <button onClick={this.add}>Plus</button>
+      <button onClick={this.minus}>Minus</button>
     </div>
     )
   }
-    
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Button from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Button />
   </React.StrictMode>,
   document.getElementById('root')
 );
